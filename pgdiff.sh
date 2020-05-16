8311a0cb124bf85b2cb456427911ccc3a2c5f1a4
prtha112 <thahi5_vi@hotmail.com>
2020-05-16 23:23:44 +0700

    Add install remote for ubuntu

---
+++ pgdiff.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo $iam
+md5_server=$(curl https://raw.githubusercontent.com/prtha112/pgdiff/master/pgdiff | md5sum | awk '{print $1}' | tail -1)
+
+echo "---------------------------------------"
+echo $md5_server
+
+rm /usr/local/bin/pgdiff 2> /dev/null
+
+wget -P /usr/local/bin/ https://raw.githubusercontent.com/prtha112/pgdiff/master/pgdiff
+md5_local=$(cat /usr/local/bin/pgdiff | md5sum | awk '{print $1}')
+
+echo $md5_local
+
+if [ "$md5_server" != "$md5_local" ] 
+then
+	rm /usr/local/bin/pgdiff
+	echo "Install not success."
+	exit
+fi
+
+chmod 755 /usr/local/bin/pgdiff
