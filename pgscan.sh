543d9de889224daf5446d66a17eb66c74c3ed886
prtha112 <thahi5_vi@hotmail.com>
2020-05-17 14:45:31 +0700

    Fix bug installer

--- pgscan.sh
+++ pgscan.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-md5_server=$(curl https://raw.githubusercontent.com/prtha112/pgscan/master/pgdiff | md5sum | awk '{print $1}' | tail -1)
+md5_server=$(curl https://raw.githubusercontent.com/prtha112/pgscan/master/pgscan | md5sum | awk '{print $1}' | tail -1)
 
 echo "---------------------------------------"
 echo $md5_server
 
 rm /usr/local/bin/pgscan 2> /dev/null
 
-wget -P /usr/local/bin/ https://raw.githubusercontent.com/prtha112/pgscan/master/pgdiff
+wget -P /usr/local/bin/ https://raw.githubusercontent.com/prtha112/pgscan/master/pgscan
 md5_local=$(cat /usr/local/bin/pgscan | md5sum | awk '{print $1}')
 
 echo $md5_local
