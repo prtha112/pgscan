592b38f0aea4f0ca6d97bd5c78b68ba79f53be16
prtha112 <thahi5_vi@hotmail.com>
2020-05-17 14:35:18 +0700

    Change local file to pgscan

--- pgscan.sh
+++ pgscan.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-md5_server=$(curl https://raw.githubusercontent.com/prtha112/pgdiff/master/pgdiff | md5sum | awk '{print $1}' | tail -1)
+md5_server=$(curl https://raw.githubusercontent.com/prtha112/pgscan/master/pgdiff | md5sum | awk '{print $1}' | tail -1)
 
 echo "---------------------------------------"
 echo $md5_server
 
-rm /usr/local/bin/pgdiff 2> /dev/null
+rm /usr/local/bin/pgscan 2> /dev/null
 
-wget -P /usr/local/bin/ https://raw.githubusercontent.com/prtha112/pgdiff/master/pgdiff
-md5_local=$(cat /usr/local/bin/pgdiff | md5sum | awk '{print $1}')
+wget -P /usr/local/bin/ https://raw.githubusercontent.com/prtha112/pgscan/master/pgdiff
+md5_local=$(cat /usr/local/bin/pgscan | md5sum | awk '{print $1}')
 
 echo $md5_local
 
 if [ "$md5_server" != "$md5_local" ] 
 then
-	rm /usr/local/bin/pgdiff
+	rm /usr/local/bin/pgscan
 	echo "Install not success."
 	exit
 fi
 
-chmod 755 /usr/local/bin/pgdiff
+chmod 755 /usr/local/bin/pgscan
 echo "Install successfully."
